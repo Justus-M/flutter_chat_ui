c12d1dbe01ecebbfb299819ea2471bbdf034f63e
Justus <37046316+Justus-M@users.noreply.github.com>
2022-03-16 08:12:06 +0100

    Update chat_list.dart

--- lib/src/widgets/chat_list.dart
+++ lib/src/widgets/chat_list.dart
@@ -219,7 +219,7 @@ class _ChatListState extends State<ChatList>
         reverse: true,
         slivers: [
           SliverPadding(
-            padding: const EdgeInsets.only(bottom: 4),
+            padding: const EdgeInsets.only(bottom: 150),
             sliver: SliverAnimatedList(
               initialItemCount: widget.items.length,
               key: _listKey,
